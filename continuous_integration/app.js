27ca9eacbcbf89f4524826088b8a9c8cbe70549c
Rupali <rupali.rode@gmail.com>
2023-12-18 12:00:55 +0530

    3

--- continuous_integration/app.js
+++ continuous_integration/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({message:'This s my first project  node test and CICD and Webhook.'});
+  res.status(200).json({ message: 'This will demo node test and CICD and Webhook.' });
 });
 
 module.exports = app;
