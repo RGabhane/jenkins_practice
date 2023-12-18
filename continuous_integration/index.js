2e32e15caf2ce93702d0ea657a7289e578f1161d
Rupali <rupali.rode@gmail.com>
2023-12-18 11:52:40 +0530

    2

--- continuous_integration/index.js
+++ continuous_integration/index.js
@@ -3,5 +3,5 @@ const app = require("./app")
 // strating application on port 3000
 const port = 3000;
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`server is not  running on port ${port}`);
 });
